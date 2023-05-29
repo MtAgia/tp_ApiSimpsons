84fcd6615ae16f2b88dd9b2341b6a3351e290f9b
Emilse Arias <emilsearias@gmail.com>
2023-05-29 18:36:26 -0300

    agregamos la peticion a la api de los simpsons usando fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(()=>{
+    consultarAPI();
+  }, [])
+
+const consultarAPI = async ()=>{
+  //vamos hacer una peticion GET
+  const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+  const datos = await respuesta.json();
+  console.log(respuesta);
+  console.log(datos[0]);
+  setPersonaje(datos[0])
+}
+
 
   return (
     <>
